55f84abb9dde17733aab4905378c3fbfaf1cf765
Tptrix <tptrix@best.net.ua>
2019-08-05 14:09:54 +0300

    Add file;

--- public/index.php
+++ public/index.php
@@ -7,12 +7,13 @@ require '../vendor/autoload.php';
 
 $client = new Client();
 define('TOKEN', '711874680:AAGjVbWznVJOoV-3PpTDnxC-IbqaXRICb9Y');
+define('URL', 'https://api.telegram.org/bot' . TOKEN . '/');
 
-//$body = $client
-//    ->get('https://api.telegram.org/bot' . TOKEN . '/getUpdates')
-//    ->getBody()
-//;
-//
-//print_r(json_decode($body));
+$body = $client->post(URL . 'setWebhook', [
+    'multipart' => [
+        'name'     => 'certificate',
+        'contents' => file_get_contents(''),
+    ],
+])->getBody();
 
-syslog(LOG_ERR, print_r(json_decode(file_get_contents('php://input')), true));
+syslog(LOG_ERR, print_r($body, true));
