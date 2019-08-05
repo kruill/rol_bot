04c1e4b521e43ac4225569664124c41e071008a7
Tptrix <tptrix@best.net.ua>
2019-08-05 15:28:57 +0300

    Add more features;

--- public/index.php
+++ public/index.php
@@ -45,18 +45,29 @@ if ($body->message->text == '/roll') {
         $txt = 'Ахуел?';
     }
 
+    if ($rand == 99) {
+        $client->post(URL . 'sendPhoto', [
+            'json' => [
+                'chat_id' => $chat->id,
+                'photo'   => "https://memepedia.ru/wp-content/uploads/2017/04/%D0%B5%D0%B1%D0%B0%D1%82%D1%8C-%D1%82%D1%8B-%D0%BB%D0%BE%D1%85-%D0%BE%D1%80%D0%B8%D0%B3%D0%B8%D0%BD%D0%B0%D0%BB.jpg",
+            ],
+        ]);
+    }
+
     $client->post(URL . 'sendMessage', [
         'json' => [
-            'chat_id' => $chat->id,
-            'text'    => "{$rand} {$txt}",
+            'chat_id'             => $chat->id,
+            'text'                => "{$rand} {$txt}",
+            'reply_to_message_id' => $body->message->message_id,
         ],
     ]);
 } else {
     if ($body->message->from->first_name == 'Гримлий') {
         $client->post(URL . 'sendMessage', [
             'json' => [
-                'chat_id' => $chat->id,
-                'text'    => 'Завали ебало!',
+                'chat_id'             => $chat->id,
+                'text'                => 'Завали ебало!',
+                'reply_to_message_id' => $body->message->message_id,
             ],
         ]);
     }
