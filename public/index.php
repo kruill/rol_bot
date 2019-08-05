724d4f68cf4f1eb47285fdae544f2788d8f04c10
Tptrix <tptrix@best.net.ua>
2019-08-05 15:42:18 +0300

    Add more features;

--- public/index.php
+++ public/index.php
@@ -29,20 +29,39 @@ if ($body->message->text == '/roll') {
     $rand = rand(0, 100);
 
     $txt = '';
-    if ($rand % 2) {
-        $txt = 'Пизда те жирный!';
-    }
-
-    if (primeCheck($rand)) {
-        $txt = 'Простое число, всех уебал, бежим!';
-    }
 
     if ($rand == 0) {
-        $txt = 'Тобi пiзда, тiкай з хаты!';
+        $txt = 'Ебать ты лох, убил сам себя!';
+        $client->post(URL . 'sendPhoto', [
+            'json' => [
+                'chat_id' => $chat->id,
+                'photo'   => "https://memepedia.ru/wp-content/uploads/2017/04/%D0%B5%D0%B1%D0%B0%D1%82%D1%8C-%D1%82%D1%8B-%D0%BB%D0%BE%D1%85-%D0%BE%D1%80%D0%B8%D0%B3%D0%B8%D0%BD%D0%B0%D0%BB.jpg",
+            ],
+        ]);
+    }
+
+    if ($rand > 0 && $rand <= 10) {
+        $txt = 'От же дидько, сам себя решил пиздануть!';
+    }
+
+    if ($rand > 10 && $rand <= 20) {
+        $txt = 'Промах, лох!';
+    }
+
+    if ($rand > 20 && $rand <= 80) {
+        $txt = 'Думаэшь?';
+    }
+
+    if ($rand > 80 && $rand <= 90) {
+        $txt = 'Кританул, пидор!';
+    }
+
+    if ($rand > 90 && $rand <= 98) {
+        $txt = 'Кританул, обоссышь, двойной урон!';
     }
 
     if ($rand == 100) {
-        $txt = 'Ахуел?';
+        $txt = 'Убил к хуям, афелка!';
     }
 
     if ($rand == 99) {
