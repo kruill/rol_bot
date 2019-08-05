44863905bef262cb391e02aad16d689df3064667
Tptrix <tptrix@best.net.ua>
2019-08-05 18:00:53 +0300

    fix;

--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@ if (isset($body->message->entities) && is_array($body->message->entities)) {
     $mentioned = $body->message->entities[0]->user->first_name ?? '';
 }
 
-syslog(LOG_ERR, var_dump($mentioned, true));
+syslog(LOG_ERR, var_export($mentioned, true));
 syslog(LOG_ERR, print_r($body->message->entities, true));
 
 $get_damn = function (string $name) use ($client) {
