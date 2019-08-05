5718c764eef9674922986085c3ddf39ba56f7306
Tptrix <tptrix@best.net.ua>
2019-08-05 15:32:15 +0300

    Add more features;

--- public/index.php
+++ public/index.php
@@ -52,6 +52,8 @@ if ($body->message->text == '/roll') {
                 'photo'   => "https://memepedia.ru/wp-content/uploads/2017/04/%D0%B5%D0%B1%D0%B0%D1%82%D1%8C-%D1%82%D1%8B-%D0%BB%D0%BE%D1%85-%D0%BE%D1%80%D0%B8%D0%B3%D0%B8%D0%BD%D0%B0%D0%BB.jpg",
             ],
         ]);
+
+        $txt = 'Ебать ты лох!';
     }
 
     $client->post(URL . 'sendMessage', [
