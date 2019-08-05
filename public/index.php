29795f8a0014487a949675d0765c54a5fe086368
Tptrix <tptrix@best.net.ua>
2019-08-05 17:37:55 +0300

    Add new feature;

--- public/index.php
+++ public/index.php
@@ -13,35 +13,46 @@ define('URL', 'https://api.telegram.org/bot' . TOKEN . '/');
 $body = json_decode(file_get_contents('php://input'));
 $chat = $body->message->chat;
 
-function primeCheck($number) {
-    if ($number == 1) {
-        return 0;
-    }
-    for ($i = 2; $i <= $number / 2; $i++) {
-        if ($number % $i == 0) {
-            return 0;
-        }
-    }
-
-    return 1;
+if ($body->message->text == 'asd') {
+    $client->post(URL . 'sendMessage', [
+        'json' => [
+            'chat_id'             => $chat->id,
+            'text'                => $body,
+            'reply_to_message_id' => $body->message->message_id,
+        ],
+    ]);
 }
 
-if ($body->message->text == '/try') {
-    $body = $client->get('https://damn.ru/?name=' . $body->message->from->first_name . '&sex=m')->getBody();
-    $txt = html_entity_decode(strip_tags((string)str_get_html($body)->find('div[class=damn]')[0]));
+$get_damn = function (string $name) use ($client) {
+    $body = $client->get('https://damn.ru/?name=' . $name . '&sex=m')->getBody();
+
+    return html_entity_decode(strip_tags((string)str_get_html($body)->find('div[class=damn]')[0]));
+};
+
+if (strpos($body->message->text, '/try')) {
+    $mentioned = $body->message->entities[0]->user->first_name ?? '';
+    $user = $body->message->from->first_name;
+    if ($mentioned) {
+        $user = $mentioned;
+    }
 
     $client->post(URL . 'sendMessage', [
         'json' => [
             'chat_id'             => $chat->id,
-            'text'                => $txt,
+            'text'                => $get_damn($user),
             'reply_to_message_id' => $body->message->message_id,
         ],
     ]);
 } else {
     if ($body->message->text == '/roll') {
         $rand = rand(0, 100);
-
         $txt = '';
+
+        $mentioned = $body->message->entities[0]->user->first_name ?? '';
+        $user = $body->message->from->first_name;
+        if ($mentioned) {
+            $user = $mentioned;
+        }
 
         if ($rand == 0) {
             $txt = 'Ебать ты лох, убил сам себя!';
@@ -51,30 +62,12 @@ if ($body->message->text == '/try') {
                     'photo'   => "https://memepedia.ru/wp-content/uploads/2017/04/%D0%B5%D0%B1%D0%B0%D1%82%D1%8C-%D1%82%D1%8B-%D0%BB%D0%BE%D1%85-%D0%BE%D1%80%D0%B8%D0%B3%D0%B8%D0%BD%D0%B0%D0%BB.jpg",
                 ],
             ]);
-        }
-
-        if ($rand > 0 && $rand <= 10) {
-            $txt = 'От же дидько, сам себя решил пиздануть!';
-        }
-
-        if ($rand > 10 && $rand <= 20) {
-            $txt = 'Промах, лох!';
-        }
-
-        if ($rand > 20 && $rand <= 80) {
-            $txt = 'Думаэшь?';
-        }
-
-        if ($rand > 80 && $rand <= 90) {
-            $txt = 'Кританул, пидор!';
-        }
-
-        if ($rand > 90 && $rand <= 98) {
-            $txt = 'Кританул, обоссышь, двойной урон!';
-        }
-
-        if ($rand == 100) {
-            $txt = 'Убил к хуям, афелка!';
+        } else {
+            if ($rand == 100) {
+                $txt = 'Убил к хуям, афелка!';
+            } else {
+                $txt = $get_damn($user);
+            }
         }
 
         if ($rand == 99) {
